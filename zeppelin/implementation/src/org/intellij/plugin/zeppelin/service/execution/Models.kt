6c830b13ca7bbf6b8ae1c761ef8efe7b17e7b043
Nikita Ashikhmin <veoring@gmail.com>
2018-10-04 17:07:57 +0300

    Rewrite Execution handlers with coroutines

--- zeppelin/implementation/src/org/intellij/plugin/zeppelin/service/execution/Models.kt
+++ zeppelin/implementation/src/org/intellij/plugin/zeppelin/service/execution/Models.kt
@@ -3,15 +3,17 @@ package org.intellij.plugin.zeppelin.service.execution
 import org.intellij.plugin.zeppelin.models.ZeppelinException
 import org.intellij.plugin.zeppelin.utils.JsonParser
 
-data class ProgressResponse(val id: String, val progress: Int)
 
-data class ParagraphResponse(val id: String, val status: String, val results: ExecutionResults?)
+sealed class ExecutionResponse
+data class ProgressResponse(val id: String, val progress: Int) : ExecutionResponse()
 
-data class ExecutionResults(val code: String, val msg: List<ExecutionResultsMsg>)
+data class ParagraphResponse(val id: String, val status: String, val results: ExecutionResults?) : ExecutionResponse()
 
-data class ExecutionResultsMsg(val type: String="", val data: String)
+data class ExecutionResults(val code: String, val msg: List<ExecutionResultsMsg>) : ExecutionResponse()
 
-data class OutputResponse(val data: String, val index: Int, val noteId: String, val paragraphId: String)
+data class ExecutionResultsMsg(val type: String="", val data: String): ExecutionResponse()
+
+data class OutputResponse(val data: String, val index: Int, val noteId: String, val paragraphId: String): ExecutionResponse()
 
 object ExecutionModelConverter {
     fun getOutputResult(json: Any): OutputResponse {
--- zeppelin/implementation/src/org/intellij/plugin/zeppelin/service/execution/TaskExecutor.kt
+++ zeppelin/implementation/src/org/intellij/plugin/zeppelin/service/execution/TaskExecutor.kt
@@ -1,5 +1,12 @@
 package org.intellij.plugin.zeppelin.service.execution
 
+import kotlinx.coroutines.experimental.channels.Channel
+import kotlinx.coroutines.experimental.channels.SendChannel
+import kotlinx.coroutines.experimental.channels.actor
+import kotlinx.coroutines.experimental.delay
+import kotlinx.coroutines.experimental.launch
+import kotlinx.coroutines.experimental.runBlocking
+import org.intellij.plugin.zeppelin.models.ZeppelinException
 import java.util.concurrent.atomic.AtomicBoolean
 
 /**
@@ -9,13 +16,68 @@ import java.util.concurrent.atomic.AtomicBoolean
  * @param outputHandler - a handler which get executions results
  */
 class TaskExecutor(val id: String, private val outputHandler: ExecutionHandler) {
+    private val taskActor = taskActor()
+
+    init {
+        delayedResponseHandle()
+    }
+
     fun isCompleted(): AtomicBoolean = outputHandler.isCompleted()
 
-    fun appendOutput(output: OutputResponse) = outputHandler.onOutput(output, true)
+    fun appendOutput(output: OutputResponse) = runBlocking<Unit> {
+        val time = System.currentTimeMillis()
+        launch {
+            taskActor.send(ResponseActorMsg(time, output))
+        }
+    }
 
     fun progress(progressResponse: ProgressResponse) = outputHandler.onProgress(progressResponse.progress / 100.0)
 
-    fun paragraph(paragraphResponse: ParagraphResponse) {
+    fun paragraph(paragraphResponse: ParagraphResponse) = runBlocking<Unit> {
+        val time = System.currentTimeMillis()
+        launch {
+            taskActor.send(ResponseActorMsg(time, paragraphResponse))
+        }
+    }
+
+    fun updateOutput(output: OutputResponse) = runBlocking<Unit> {
+        val time = System.currentTimeMillis()
+        launch {
+            taskActor.send(ResponseActorMsg(time, output))
+        }
+    }
+
+    private fun delayedResponseHandle() = runBlocking<Unit> {
+        launch {
+            while (!isCompleted().get()) {
+                val responseChannel = Channel<List<ResponseActorMsg>>()
+                taskActor.send(GetResponseMsg(responseChannel))
+                val received = responseChannel.receive()
+                handleResponses(received)
+                delay(1000L)
+            }
+            taskActor.close()
+        }
+    }
+
+    private fun handleResponses(responses: List<ResponseActorMsg>) {
+        if (responses.isEmpty()) return
+        val sortedResponses = responses.sortedBy { it.time }.map { it.response }
+        val filtered = sortedResponses.zipWithNext().filter { it.first != it.second }
+        val responsesCleaned = listOf(sortedResponses.first()) + filtered.map { it.second }
+
+        //TODO: this module should to say when the process is eng GUI is just dummy dependent output handler
+        responsesCleaned.forEach {
+            when (it) {
+                is ParagraphResponse -> handleParagraphResponse(it)
+                is OutputResponse -> outputHandler.onOutput(it, true)
+                else -> throw ZeppelinException("Unhandled result")
+            }
+        }
+    }
+
+    private fun handleParagraphResponse(
+            paragraphResponse: ParagraphResponse) {
         val status: String = paragraphResponse.status
         outputHandler.onUpdateExecutionStatus(status)
         when (status) {
@@ -30,7 +92,25 @@ class TaskExecutor(val id: String, private val outputHandler: ExecutionHandler) 
         }
     }
 
-    fun updateOutput(output: OutputResponse) {
-        outputHandler.onOutput(output, false)
+    private fun taskActor() = actor<ActorMsg> {
+        val results: MutableList<ResponseActorMsg> = mutableListOf()
+        for (msg in channel) { // iterate over incoming messages
+            when (msg) {
+                is ResponseActorMsg -> results.add(msg)
+                is GetResponseMsg -> {
+                    val time = System.currentTimeMillis()
+                    val (old, fresh) = results.partition { time - it.time > 1000 }
+                    msg.response.send(old.toList())
+                    results.clear()
+                    results.addAll(fresh)
+                }
+            }
+        }
     }
 }
+
+private sealed class ActorMsg
+
+private class ResponseActorMsg(val time: Long, val response: ExecutionResponse) : ActorMsg()
+
+private class GetResponseMsg(var response: SendChannel<List<ResponseActorMsg>>) : ActorMsg()
